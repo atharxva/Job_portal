91ca3f4c84a1c57dc244945a27fd5827ef5b21d6
hatim0202 <hatimmandviwala7@gmail.com>
2025-11-08 23:57:18 +0530

    Initial commit: Full-stack MERN app with Vercel configuration

---
+++ LinkedIn/frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Add this 'server' block
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // <-- This is your local backend port
+        changeOrigin: true,
+      }
+    }
+  }
+})
