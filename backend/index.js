b90aa4aa8efd6a538dd11924376d3df761634181
atharvajadhav-19 <atharva.jadhav@winvesta.in>
2026-02-06 10:46:52 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -63,7 +63,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
